8058549e785ddede36183acc7e3515747fe0c463
dernando <dernandonc@gmail.com>
2022-06-01 18:00:14 -0300

    implementa método de notificação

--- src/event/@shared/event-dispatcher.ts
+++ src/event/@shared/event-dispatcher.ts
@@ -12,7 +12,12 @@ export default class EventDispatcher implements EventDispatcherInterface {
 
 
     notify(event: eventInterface): void {
-        throw new Error("Method not implemented.");
+        const eventName = event.constructor.name;
+        if (this.eventHandlers[eventName]) {
+          this.eventHandlers[eventName].forEach((eventHandler) => {
+            eventHandler.handle(event);
+          });
+        }
     }
     register(eventName: string, eventHandler: eventHandlerInterface<eventInterface>): void {
         if (!this.eventHandlers[eventName]) {
