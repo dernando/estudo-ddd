073998ddd6389f11a2eee419c61ea0fc373c2cbb
dernando <dernandonc@gmail.com>
2022-06-01 17:44:06 -0300

    adiciona teste de cancelamento ed um registro

--- src/event/@shared/event-dispacther.spec.ts
+++ src/event/@shared/event-dispacther.spec.ts
@@ -23,4 +23,24 @@ describe("Domain events tests", () => {
 
     });
 
+    it("should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    
+        expect(
+          eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+        ).toMatchObject(eventHandler);
+    
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+    
+        expect(
+          eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+        ).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(
+          0
+        );
+      });
+
 });
